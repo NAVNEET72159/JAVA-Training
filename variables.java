ef86824587ee952b9cf40053d43e418d03f3d7dd
Navneet Shandilya <30992372+NAVNEET72159@users.noreply.github.com>
2023-07-31 13:32:38 +0530

    first commit

---
+++ variables.java
@@ -0,0 +1,10 @@
+public class variables {
+    public static void main(String[] args) {
+        // Declare a variable
+        int myNumber;
+        // Assign a value to the variable
+        myNumber = 5;
+        // Print the variable
+        System.out.println(myNumber);
+    }
+}
